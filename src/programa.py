e97cf2fb6574fc5410bdcc57b4cbc0c8eb795231
Progclasdani <daniel_vicente_@outlook.es>
2026-01-15 20:46:10 +0100

    Update programa.py
    Added basic comments to the code and a basic function for the menu.

--- src/programa.py
+++ src/programa.py
@@ -1,1 +1,23 @@
+#Prototipo programa
+"""
+Daniel Vicente Moya 15/01/2025
+*********************************************
+Programa para la toma de datos de puntos y
+su representación en una gráfica.
+*********************************************
+"""
+#Importado de módulos
 
+#Declaración de funciones
+def menu_programa():
+    """
+    Función que muestra las opciones disponibles
+    en un menú.
+    """
+    print("Opciones disponibles:")
+    print("1- Añadir puntos.")
+    print("2- Muestra diagrama de puntos.")
+    print("3- Cerrar programa.")
+    return
+
+#Programa principal
